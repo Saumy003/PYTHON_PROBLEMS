6ddb0cfe1732e5e04f45e3b1f591b14fff2a3f1f
Saumy003 <saumysingh2005@gmail.com>
2026-02-09 01:26:00 +0530

    problem4

---
+++ program4.py
@@ -0,0 +1,22 @@
+## WAP to swap two number
+
+# method1(using 3rd variable)
+
+x = 13
+y = 12
+
+temp = x
+x = y
+y = temp
+
+print("the value of x is:" , x)
+print("the value of y is:" , y)
+
+#method2(without using 3rd variable)
+
+a = 4
+b = 5
+
+a , b = b , a
+print("the value of a is:" , a)
+print("the value of b is:" , b)
