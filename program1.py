d6669e27da6a68685417d0952eb004cbd65f50c2
Saumy003 <saumysingh2005@gmail.com>
2026-02-09 01:19:10 +0530

    problem1

---
+++ program1.py
@@ -0,0 +1,10 @@
+## check wheather given number is prime or not
+
+num = int(input("enter the number:"))
+
+for i in range(2,num):
+    if(num % i == 0):
+        print("not prime")
+        break
+else:
+    print("prime")
