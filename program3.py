19456163c86e303ff697a09d42c6faebef8006f2
Saumy003 <saumysingh2005@gmail.com>
2026-02-09 01:25:24 +0530

    problem3

---
+++ program3.py
@@ -0,0 +1,8 @@
+## calculate the area of a triangle
+
+base = float(input("enter the base of the triangle:"))
+height = float(input("enter the height of the triangle:"))
+
+area = 0.5 * (base * height)
+
+print("required area of the triangle is:" , area)
