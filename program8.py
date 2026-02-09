3d2e2bd5cb4f49004c7072a03da0949ff6fb7809
Saumy003 <saumysingh2005@gmail.com>
2026-02-10 00:00:50 +0530

    problem8

---
+++ program8.py
@@ -0,0 +1,10 @@
+# to cheak either given number is positive , negative or zero
+
+num = int(input("enter the number:"))
+
+if(num>0):
+    print("given number is positive")
+elif(num<0):
+    print("given number is negative")
+else:
+    print("given number is zero")
