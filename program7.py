b6b44e7a5f466a4513e15fa469c97841572447ed
Saumy003 <saumysingh2005@gmail.com>
2026-02-09 23:49:46 +0530

    problem7

---
+++ program7.py
@@ -0,0 +1,12 @@
+## WAP to find greatest of three number
+
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+c = int(input("enter third number:"))
+
+if(a>b and a>c):
+    print("a is greatest")
+elif(b>a and b>c):
+    print("b is greatest")
+else:
+    print("c is greatest")
