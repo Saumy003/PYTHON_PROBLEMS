2e6121f3c91b54c1446abd2dbc83028b61a7e521
Saumy003 <saumysingh2005@gmail.com>
2026-02-14 00:39:24 +0530

    problem12

--- program12.py
+++ program12.py
@@ -1,2 +1,19 @@
+#Compute the factorial of a number using for loop.
+
+number = 5
+factorial = 1
+
+for i in range(1,6):
+    factorial = factorial * i
+print(factorial)
+
 #Compute the factorial of a number using while loop.
 
+num_ber = 6
+factorial = 1
+
+while (num_ber >0) :
+    factorial = factorial * num_ber
+    num_ber -= 1
+
+print(factorial)
