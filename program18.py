7c2f2cb8b744d7be09c530b5216f3e429f866e26
Saumy003 <saumysingh2005@gmail.com>
2026-02-23 22:47:07 +0530

    problem18

---
+++ program18.py
@@ -0,0 +1,11 @@
+# Write a program to find factorial of a number using recursion.
+
+    
+def factorial (n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n-1)
+
+num = int(input("Enter the number:"))
+
+print("Factorial =" , factorial(num))
