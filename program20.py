dbb35496f423fd7e903e05c1d81ecd2382b6d350
Saumy003 <saumysingh2005@gmail.com>
2026-02-28 22:10:15 +0530

    problem 20

--- program20.py
+++ program20.py
@@ -1,2 +1,13 @@
-# Write a Python program to print the Fibonacci sequence using while loop.
+# Write a Python program to print the Fibonacci sequence using recursion.
 
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+# number of terms
+terms = int(input("Enter number of terms: "))
+
+print("Fibonacci Series:")
+for i in range(terms):
+    print(fibonacci(i), end=" ")
