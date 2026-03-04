e1200ba685d85a1b0699a647ee9a38527584fb7a
Saumy003 <saumysingh2005@gmail.com>
2026-03-04 23:24:02 +0530

    problem 22

--- program22.py
+++ program22.py
@@ -1,1 +1,17 @@
-# Program22
+# WAP a program to check if the number is Armstrong or Not?
+
+num = int(input("enter the number:"))
+order = len(str(num))
+
+sum = 0
+temp = num
+
+while temp >0:
+    digit = temp % 10
+    sum += digit ** order
+    temp //= 10
+
+if sum == num:
+    print("it is an armstrong number")
+else:
+    print("it is not an armstrong nnumber")
