0e80f113aaab391b35b885303de518a3b43b1b32
Saumy003 <saumysingh2005@gmail.com>
2026-03-22 18:21:08 +0530

    problem24

--- program24.py
+++ program24.py
@@ -1,1 +1,18 @@
-# Program 24
+# Itration Key Values in Dictonary
+
+my_dict = {
+    "name" : "Anirudh",
+    "age" : 22,
+    "gender" : "Male"
+}
+
+for k in my_dict.keys():
+    print(my_dict[k])
+# both will print same things
+for v in my_dict.values():
+    print(v)
+
+# print output like this: name -> Anirudh
+for k ,v in my_dict.items():
+    print(f"{k} -> {v}")
+    
