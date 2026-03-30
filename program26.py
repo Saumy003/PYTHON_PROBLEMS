bf4c3427a235b1c91368d69f9b70d266ddb31e22
Saumy003 <saumysingh2005@gmail.com>
2026-03-30 10:25:59 +0530

    sorting in python

--- program26.py
+++ program26.py
@@ -1,1 +1,14 @@
-#prroogram26
+# SORTING ALGO #
+
+# 1. BUBBLE SORT
+nums = [ 5, 2, 9, 7, 1, 3, 6]
+n = len(nums)
+
+for i in range(n-2, -1, -1):
+    for j in range(0, i+1):
+        if nums[j]> nums[j+1]:
+            nums[j], nums[j+1] = nums[j+1], nums[j]
+
+print("Sorted array:",nums)
+
+#2. Selection Sort
