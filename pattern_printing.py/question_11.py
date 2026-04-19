1719939260433fe238beafde755697240c537a02
Saumy003 <saumysingh2005@gmail.com>
2026-04-19 23:46:54 +0530

    pattern printing problems

---
+++ pattern_printing.py/question_11.py
@@ -0,0 +1,17 @@
+"""
+Print this given pattern => 
+
+          *
+        * * *
+      * * * * *
+    * * * * * * *
+    
+"""
+
+n = int(input("enter the value of n"))   # here n =4
+for i in range(1, n+1):
+    for j in range(1, (n-i)+1):
+        print(" ", end=" ")
+    for k in range(1, 2*i):
+        print("*", end=" ")
+    print()
