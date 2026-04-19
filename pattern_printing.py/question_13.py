c7361203ce05caf0a869a074044d8479d71bc5a4
Saumy003 <saumysingh2005@gmail.com>
2026-04-20 01:23:51 +0530

    pattern printing problems

--- pattern_printing.py/question_13.py
+++ pattern_printing.py/question_13.py
@@ -1,10 +1,17 @@
 """
 Print this given pattern => 
 
-1
-2 3
-4 5 6
-7 8 9 10
-11 12 13 14 15
- 
+1 2 3 4 5
+  1 2 3 4
+    1 2 3
+      1 2
+        1
+
 """
+n = 5
+for i in range(1, n+1):
+    for j in range(1, i):
+        print(" ", end=" ")
+    for k in range(1, (n-i)+2):
+        print(k, end=" ")
+    print()
